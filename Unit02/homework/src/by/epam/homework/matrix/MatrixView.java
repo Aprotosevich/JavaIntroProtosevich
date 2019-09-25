54dfd429e2e1901bbe435bc3d2ae1a4b383d0d3d
Antony Protosevich <antony.protosevich@gmail.com>
2019-09-25 16:30:09 +0300

    task23-34 added to matrix block

--- Unit02/homework/src/by/epam/homework/matrix/MatrixView.java
+++ Unit02/homework/src/by/epam/homework/matrix/MatrixView.java
@@ -20,6 +20,14 @@ public class MatrixView {
         System.out.println();
     }
 
+    public void printDoubleMatrix(double[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(" " + matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
 
 
